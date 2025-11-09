ae54e8ded3c80d9c42b67d9114bd4d3a7be47659
muskan-focusofthq <muskan@focusofthq.com>
2025-11-10 03:35:24 +0530

    Fix: Update seeder to not use factory (Heroku compatibility)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
 
         $this->call([
